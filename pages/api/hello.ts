5d0d44400816f5fab59ec5854844906da33bd251
code123841 <udhay1co.de@gmail.com>
2022-02-18 20:03:34 +0530

    api folders
    Co-authored-by: moni <monitrr@users.noreply.github.com>

---
+++ pages/api/hello.ts
@@ -0,0 +1,3 @@
+export default function handler(_req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { name: string; }): void; new(): any; }; }; }) {
+    res.status(200).json({ name: 'John Doe' })
+  }
