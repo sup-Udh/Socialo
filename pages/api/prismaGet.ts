22a23227cd1012283ebcb1082473ec345bba3011
code123841 <udhay1co.de@gmail.com>
2022-02-22 14:38:21 +0530

    Test Passed. :)

--- pages/api/prismaGet.ts
+++ pages/api/prismaGet.ts
@@ -2,16 +2,22 @@
   import prisma from  "../../prisma/prisma";
   
   // async fucntion named as test 
+//   export default 
+export default function PrismaReqHandler(){
 
-  async function Test(){
-    const user = await prisma.user.findUnique({
-      where: {
-        id: 'ckzwfx6510006o0dorc3xdqfg',
-      },
-    })
-
-    console.log(user)
     
-  }
+    async function Test(){
+        const user = await prisma.user.findUnique({
+          where: {
+            id: 'ckzwfx6510006o0dorc3xdqfg',
+          },
+        })
+    
+        console.log(user)
+        
+      }
+    
+      Test()
+    
 
-  Test()
+}
