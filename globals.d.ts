d7747f2f9ffd1c0977270cc03bc705321b619594
code123841 <udhay1co.de@gmail.com>
2022-02-22 14:18:07 +0530

    Create  Prisma Clinet

--- globals.d.ts
+++ globals.d.ts
@@ -1,5 +1,8 @@
 // global.d.ts
 import { MongoClient } from "mongodb"
+import { PrismaClient } from '@prisma/client';
+
 declare global {
     var mongoClientPromise: Promise<MongoClient>;
+    var prisma: PrismaClient;
 }
