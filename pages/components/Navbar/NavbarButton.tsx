bf36e0b466e44f9dd9475d342e1efeeb9bf63e72
code123841 <udhay1co.de@gmail.com>
2022-02-26 11:34:15 +0530

    Buil padd :)

--- pages/components/Navbar/NavbarButton.tsx
+++ pages/components/Navbar/NavbarButton.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 function NavbarButton(props: {active: boolean, text: string, href: string}) {
     const { active, text, href } = props;
     if(active){
-        return (<Link passHref href={href}><h1 className="font-bold text-3xl">{text}</h1></Link>)
+        return (<Link passHref href={"/about"}><h1 className="font-bold text-3xl">{text}</h1></Link>)
     }else{
-        return (<Link passHref href={href}><h1 className="font-normal text-3xl">{text}</h1></Link>)
+        return (<Link passHref href={"/"}><h1 className="font-normal text-3xl">{text}</h1></Link>)
     }
 }
 
